0086fd373e43179e549c12350ca7a3978810b306
anshdevops <anshdevops@gmail.com>
2022-05-26 02:02:12 +0530

    Developed Basic FrONT eND

--- src/main/java/com/example/ipapp/controller/TeamController.java
+++ src/main/java/com/example/ipapp/controller/TeamController.java
@@ -4,12 +4,14 @@ import com.example.ipapp.data.entitys.Team;
 import com.example.ipapp.repository.MatchRepository;
 import com.example.ipapp.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private  TeamRepository teamRepository;
