762993d2b8455ed3f3a7d96fb20caa4012139bfc
anshdevops <anshdevops@gmail.com>
2022-05-26 11:21:27 +0530

    Added Match API

--- src/main/java/com/example/ipapp/repository/MatchRepository.java
+++ src/main/java/com/example/ipapp/repository/MatchRepository.java
@@ -3,15 +3,24 @@ package com.example.ipapp.repository;
 import com.example.ipapp.data.entitys.Match;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
-public interface MatchRepository  extends CrudRepository<Match,Long> {
+public interface MatchRepository extends CrudRepository<Match, Long> {
 
     List<Match> getByTeam1OrTeam2OrderByDateDesc(String team1, String team2, Pageable pageable);
 
-    default List<Match> findLatestMatchesByTeam(String teamName,int count){
-        return getByTeam1OrTeam2OrderByDateDesc(teamName,teamName, PageRequest.of(0,count));
+    default List<Match> findLatestMatchesByTeam(String teamName, int count) {
+        return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
     }
+
+    @Query("select m from Match  m where  (m.team1=:teamName or m.team2=:teamName) and m.date between :dateStart and :dateEnd order by date desc ")
+    List<Match> getMatchesByTeamBetweenDates(
+            @Param("teamName") String teamName,
+            @Param("dateStart") LocalDate start,
+            @Param("dateEnd") LocalDate end);
 }
